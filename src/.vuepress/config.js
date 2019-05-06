c33f2e9b1d368a66643e524d57d51ffd8a727ae4
Aries Cs <aries0d0f@gmail.com>
2019-05-06 12:30:33 +0800

    Deploy: init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'TDOHacker',
+  description: '培育台灣資安人才、促進團體交流，扶持各資安社團與讀書會永續發展。數年來舉辦數十場講座，協助多個社團與教育單位舉辦各項活動，完成多個資安教育平台專案。2016 年開始舉辦 TDOH ­PIPE、Conf等大型活動，致力打造更完善的資安學習環境。',
+  themeConfig: {
+    nav: [{
+      text: '關於我們',
+      link: '/'
+    }, {
+      text: '部落格',
+      link: '/blog'
+    }, {
+      text: '活動資訊',
+      link: '/activity'
+    }]
+  }
+}
